303ac9c334977cf01e304a6386cce320c082753b
Karyn17511 <110671351+bangke1212@users.noreply.github.com>
2026-06-30 04:13:56 +0700

    Update test.js v2 - bust cache

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,3 +1,14 @@
 exports.handler = async (event, context) => {
-  return { statusCode: 200, body: JSON.stringify({ ok: true, msg: 'Netlify functions working!' }) };
+  return { 
+    statusCode: 200, 
+    body: JSON.stringify({ 
+      ok: true, 
+      msg: 'Netlify functions working v2!',
+      env: {
+        hasThreadsAppId: !!process.env.THREADS_APP_ID,
+        hasOpenRouterKey: !!process.env.OPENROUTER_API_KEY,
+        redirectUri: process.env.THREADS_REDIRECT_URI || 'NOT SET'
+      }
+    }) 
+  };
 };
