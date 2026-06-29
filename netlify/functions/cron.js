62612e316afb7eb8403f7975ef653f3f16364af6
Karyn17511 <110671351+bangke1212@users.noreply.github.com>
2026-06-30 03:40:24 +0700

    Ultra-simple cron test

--- netlify/functions/cron.js
+++ netlify/functions/cron.js
@@ -1,28 +1,10 @@
-exports.handler = async (event, context) => {
-  try {
-    const crypto = require('crypto');
-    const requirePath = require('path');
-    
-    // Test: can we require our dependencies?
-    const { getConfig } = require(requirePath.join(__dirname, 'config.js'));
-    const config = getConfig();
-    
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        ok: true,
-        msg: 'Cron triggered',
-        configKeys: Object.keys(config).join(', '),
-        hasOpenRouterKey: !!config.openrouterApiKey
-      })
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: err.message,
-        stack: err.stack?.split('\n').slice(0, 5).join(' | ')
-      })
-    };
-  }
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      node: process.version,
+      dir: __dirname,
+      env_keys: Object.keys(process.env).filter(k => k.startsWith('THREADS') || k.startsWith('OPENROUTER'))
+    })
+  };
 };
